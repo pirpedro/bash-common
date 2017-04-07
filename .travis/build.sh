08684fe720e0642f1ee91d8fc69364b785c50fed
Pedro Rodrigues <pir.pedro@gmail.com>
2017-04-07 16:33:00 -0300

    Removing 'test_suite' branch condition.

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,7 +2,6 @@
 
 ##execute script for sintaxe test.
 $TRAVIS_BUILD_DIR/sh-common
-
-if [[ $TRAVIS_BRANCH == 'test_suite' ]]; then
-  cd $TRAVIS_BUILD_DIR/test/ && bats .
-fi
+#if [[ $TRAVIS_BRANCH == 'test_suite' ]]; then
+cd $TRAVIS_BUILD_DIR/test/ && bats .
+#fi
