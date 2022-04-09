fbbf8afe5bab27254f60c9c86dca2d736402cf5f
Pedro Rodrigues <pir.pedro@gmail.com>
2022-04-09 00:06:20 -0300

    Bump bats for new version

--- test/test_helper/helper.bash
+++ test/test_helper/helper.bash
@@ -17,5 +17,12 @@ load_lib() {
 load_lib bats-support
 load_lib bats-assert
 
-source "../bin/sh-common"
-source "../bin/sh-debug"
+# get the containing directory of this file
+# use $BATS_TEST_FILENAME instead of ${BASH_SOURCE[0]} or $0,
+# as those will point to the bats executable's location or the preprocessed file respectively
+PROJECT_ROOT="$( cd "$( dirname "$BATS_TEST_FILENAME" )/.." >/dev/null 2>&1 && pwd )"
+# make executables in src/ visible to PATH
+PATH="$PROJECT_ROOT/bin:$PATH"
+
+source "sh-common"
+source "sh-debug"
